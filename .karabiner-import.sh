b87f8074a7a6415d372cf90d76bb0530f2dc29d1
k-hamada <k.hamada.1988@gmail.com>
2016-11-24 19:08:06 +0900

    [karabiner] Init
    $ /Applications/Karabiner.app/Contents/Library/bin/karabiner export > ~/.karabiner-import.sh

---
+++ .karabiner-import.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.doublepresscommandQ 1
+/bin/echo -n .
+$cli set remap.fn2controlL 1
+/bin/echo -n .
+$cli set remap.fn2controlL_commandR2fn 1
+/bin/echo -n .
+$cli set remap.fn2controlL_optionL2fn 1
+/bin/echo -n .
+$cli set remap.fn2controlL_vm 1
+/bin/echo -n .
+$cli set remap.jis_command2eisuukana_prefer_command 1
+/bin/echo -n .
+$cli set repeat.initial_wait 200
+/bin/echo -n .
+$cli set repeat.wait 33
+/bin/echo -n .
+/bin/echo
