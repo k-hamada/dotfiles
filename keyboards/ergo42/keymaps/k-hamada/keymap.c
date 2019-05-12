53593d0d41e162a11659c85ea00fc451be9bbd42
k.hamada <k.hamada.1988@gmail.com>
2019-05-12 22:19:59 +0900

    [qmk] add keymap

---
+++ keyboards/ergo42/keymaps/k-hamada/keymap.c
@@ -0,0 +1,33 @@
+#include QMK_KEYBOARD_H
+
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+
+#define TT_LOWER TT(_LOWER)
+#define TT_RAISE TT(_RAISE)
+#define LM_LGUI LM(_LOWER, MOD_LGUI)
+#define LM_RGUI LM(_RAISE, MOD_LGUI)
+#define KanaGUI LGUI(KC_LANG1)
+#define EisuGUI LGUI(KC_LANG2)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[_QWERTY] = LAYOUT( \
+  KC_ESC  , KC_Q   , KC_W    , KC_E    , KC_R     , KC_T    , KC_LBRC , KC_RBRC , KC_Y    , KC_U     , KC_I    , KC_O    , KC_P    , KC_BSPC , \
+  KC_TAB  , KC_A   , KC_S    , KC_D    , KC_F     , KC_G    , LM_LGUI , LM_RGUI , KC_H    , KC_J     , KC_K    , KC_L    , KC_SCLN , KC_ENT  , \
+  KC_LSFT , KC_Z   , KC_X    , KC_C    , KC_V     , KC_B    , KC_LSPO , KC_RSPC , KC_N    , KC_M     , KC_COMM , KC_DOT  , KC_SLSH , KC_QUOT , \
+  KC_LCTL , KC_APP , KC_LALT , KC_LGUI , TT_LOWER , EisuGUI , KC_SPC  , KC_SPC  , KanaGUI , TT_RAISE , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT \
+),
+[_LOWER] = LAYOUT( \
+  KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_UNDS , KC_PLUS , _______ , \
+  _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , _______ , _______ , \
+  _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_LCBR , KC_RCBR , KC_PIPE , \
+  RESET   , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_MUTE , KC_VOLD , KC_VOLU , KC_EJCT \
+),
+[_RAISE] = LAYOUT( \
+  KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_MINS , KC_EQL  , _______ , \
+  _______ , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , _______ , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , _______ , _______ , \
+  _______ , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , _______ , _______ , _______ , _______ , KC_LBRC , KC_RBRC , KC_BSLS , \
+  _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_HOME , KC_PGDN , KC_PGUP , KC_END \
+)
+};
