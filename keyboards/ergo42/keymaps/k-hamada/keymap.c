27f4e597350439bfdfd9dd3d0d1c418894a47885
k.hamada <k.hamada.1988@gmail.com>
2019-05-12 22:34:13 +0900

    [qmk] edit []{}

--- keyboards/ergo42/keymaps/k-hamada/keymap.c
+++ keyboards/ergo42/keymaps/k-hamada/keymap.c
@@ -21,13 +21,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_UNDS , KC_PLUS , _______ , \
   _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , _______ , _______ , \
-  _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_LCBR , KC_RCBR , KC_PIPE , \
+  _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_LBRC , KC_RBRC , _______ , KC_PIPE , \
   RESET   , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_MUTE , KC_VOLD , KC_VOLU , KC_EJCT \
 ),
 [_RAISE] = LAYOUT( \
   KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_MINS , KC_EQL  , _______ , \
   _______ , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , _______ , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , _______ , _______ , \
-  _______ , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , _______ , _______ , _______ , _______ , KC_LBRC , KC_RBRC , KC_BSLS , \
+  _______ , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , _______ , _______ , _______ , KC_LCBR , KC_RCBR , _______ , KC_BSLS , \
   _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_HOME , KC_PGDN , KC_PGUP , KC_END \
 )
 };
