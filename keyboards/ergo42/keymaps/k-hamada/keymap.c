b7e428320e2f8c948cfcf93a8b25a7e741ea9fc7
k-hamada <k.hamada.1988@gmail.com>
2019-05-14 12:58:58 +0900

    [qmk] CMD

--- keyboards/ergo42/keymaps/k-hamada/keymap.c
+++ keyboards/ergo42/keymaps/k-hamada/keymap.c
@@ -7,9 +7,7 @@
 #define TT_LOWER TT(_LOWER)
 #define TT_RAISE TT(_RAISE)
 #define LM_LGUI LM(_LOWER, MOD_LGUI)
-#define LM_RGUI LM(_RAISE, MOD_LGUI)
-#define KanaGUI LGUI(KC_LANG1)
-#define EisuGUI LGUI(KC_LANG2)
+#define LM_RGUI LM(_RAISE, MOD_RGUI)
 
 #define PERMISSIVE_HOLD
 
@@ -18,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC  , KC_Q   , KC_W    , KC_E    , KC_R     , KC_T    , KC_LBRC , KC_RBRC , KC_Y    , KC_U     , KC_I    , KC_O    , KC_P    , KC_BSPC , \
   KC_TAB  , KC_A   , KC_S    , KC_D    , KC_F     , KC_G    , LM_LGUI , LM_RGUI , KC_H    , KC_J     , KC_K    , KC_L    , KC_SCLN , KC_ENT  , \
   KC_LSFT , KC_Z   , KC_X    , KC_C    , KC_V     , KC_B    , KC_LSPO , KC_RSPC , KC_N    , KC_M     , KC_COMM , KC_DOT  , KC_SLSH , KC_QUOT , \
-  KC_LCTL , KC_APP , KC_LALT , KC_LGUI , TT_LOWER , EisuGUI , KC_SPC  , KC_SPC  , KanaGUI , TT_RAISE , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT \
+  KC_LCTL , KC_APP , KC_LALT , KC_LGUI , TT_LOWER , KC_LCMD , KC_SPC  , KC_SPC  , KC_RCMD , TT_RAISE , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT \
 ),
 [_LOWER] = LAYOUT( \
   KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_UNDS , KC_PLUS , _______ , \
