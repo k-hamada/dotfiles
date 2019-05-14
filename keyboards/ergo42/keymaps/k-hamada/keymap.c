8907bead7301587c312291f6cd7ddbd9ff1cc2e4
k-hamada <k.hamada.1988@gmail.com>
2019-05-14 13:01:17 +0900

    [qmk] LOWER+zxcv

--- keyboards/ergo42/keymaps/k-hamada/keymap.c
+++ keyboards/ergo42/keymaps/k-hamada/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_UNDS , KC_PLUS , _______ , \
   _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , _______ , _______ , \
-  _______ , KC_UNDO , KC_CUT  , KC_COPY , KC_PSTE , _______ , _______ , _______ , KC_LCBR , KC_RCBR , KC_LBRC , KC_RBRC , _______ , KC_PIPE , \
+  _______ , G(KC_Z) , G(KC_X) , G(KC_C) , G(KC_V) , _______ , _______ , _______ , KC_LCBR , KC_RCBR , KC_LBRC , KC_RBRC , _______ , KC_PIPE , \
   RESET   , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_MUTE , KC_VOLD , KC_VOLU , KC_EJCT \
 ),
 [_RAISE] = LAYOUT( \
